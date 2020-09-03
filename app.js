4ec31ef1fe2827445ff652a3c1d41f7f98f14a78
baraneetharan <baraneetharan@live.com>
2020-09-04 02:02:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,104 @@
+const ngrok = require('ngrok');
+
+var request = require('request');
+var util = require("util");
 var restify = require('restify');
 var builder = require('botbuilder');
+var teams = require('botbuilder-teams');
 
-// Setup Restify Server
+var connector = new teams.TeamsChatConnector({
+    appId: "ea367f9a-1764-4428-ab94-3ba38b49711e",
+    appPassword: "2cnS66W-.ygY1wdyyJZk4Lj_mR_dj~2uu."
+});
+
+// const port = 8080;
+const port = process.env.PORT || 3000;
+
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+
+server.listen(port, function() {
+    console.log(`Node.js server listening on ${port}`);
+    ngrok.connect(port, function(err, url) {
+        console.log(`Node.js local server is publicly-accessible at ${url}`);
+    });
+    console.log('%s listening to %s', server.name, util.inspect(server.address()));
+
 });
 
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+// this will reset and allow to receive from any tenants
+connector.resetAllowedTenants();
 
-// Listen for messages from users 
+var bot = new builder.UniversalBot(connector);
+
+server.post('/api/composeExtension', connector.listen());
 server.post('/api/messages', connector.listen());
+server.post('/', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("Satish said: %s", session.message.text);
-});
+var composeExtensionHandler = function(event, query, callback) {
+    var attachments = [];
+    var url = "https://www.googleapis.com/books/v1/volumes?q=" + query.parameters[0].value + "&limit=100&offset=0";
+    if (query.parameters[0].value == undefined | query.parameters[0].value == '') {
+        url = "https://www.googleapis.com/books/v1/volumes?q=ISBN:9780789748591&limit=10";
+    }
+    request(url, {
+        json: true
+    }, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        var data = body;
+        for (var o of data.items) {
+            try {
+
+                console.log(o.volumeInfo.title);
+
+                var logo = {
+                    alt: o.volumeInfo.title,
+                    url: o.volumeInfo.imageLinks.thumbnail
+                };
+
+                var card = new builder.HeroCard()
+                    .title("Title: " + o.volumeInfo.title)
+                    .text("" + o.volumeInfo.description)
+                    .subtitle("Publisher: " + o.volumeInfo.publisher)
+                    .images([logo])
+                    .buttons([{
+                        type: "openUrl",
+                        title: "View Image",
+                        value: o.volumeInfo.imageLinks.thumbnail
+                    }]);
+
+                attachments.push(card.toAttachment());
+
+
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+
+        var response = teams.ComposeExtensionResponse
+            .result('list')
+            .attachments(attachments)
+            .toResponse();
+
+        // Send the response to teams
+        callback(null, response, 200);
+
+
+        //}
+    });
+
+
+};
+
+connector.onQuery('searchCmd', composeExtensionHandler);
+
+
+
+var composeInvoke = function(event) {
+    console.log(event);
+};
+
+
+connector.onInvoke('composeInvoke'); 
